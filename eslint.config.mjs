4db94d49edc902cdc38e5aea570781767df26f30
Muhamad Shidiq Maulana <shidiqmuh0@gmail.com>
2025-02-24 19:57:34 +0700

    adding page resolv

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,21 +1,9 @@
-// eslint.config.mjs
-import { FlatCompat } from "@eslint/eslintrc"
-import path from "path"
-import { fileURLToPath } from "url"
-
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-})
-
-/** @type {import('eslint').Linter.Config[]} */
-const configs = [
+export default [
   ...compat.extends("next/core-web-vitals"),
   ...compat.extends("next/typescript"),
-  
-  
-]
-
-export default configs
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": ["warn", { "argsIgnorePattern": "^_" }]
+    }
+  }
+];
